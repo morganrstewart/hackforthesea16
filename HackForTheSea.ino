c3eba5acf22d7b26e1825d73df6b5e372637b4b1
morganrstewart <morganrstewart@users.noreply.github.com>
2017-06-12 17:48:05 -0400

    Added Circuit Playground script
    Checks sensors and prints to serial.

---
+++ HackForTheSea.ino
@@ -0,0 +1,25 @@
+#include <Adafruit_CircuitPlayground.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial ser(0,1);
+
+int pin = 10;
+void setup() {
+  // put your setup code here, to run once:
+  ser.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  ser.print(String(CircuitPlayground.temperature()) + ", ");
+  //Serial.print(String(CircuitPlayground.temperature()) + ", ");
+  ser.print(String(CircuitPlayground.readCap(pin)) + ", ");
+  //Serial.print(String(CircuitPlayground.readCap(pin)) + ", ");
+  ser.println(String(CircuitPlayground.lightSensor()) + ", ");
+  //Serial.println(String(CircuitPlayground.lightSensor()) + ", ");
+  delay(1000);
+  //Serial.print(String(CircuitPlayground.motionX()) + ", ");
+  //Serial.print(String(CircuitPlayground.motionY()) + ", ");
+  //Serial.print(String(CircuitPlayground.motionZ()) + ", ");
+  
+}
